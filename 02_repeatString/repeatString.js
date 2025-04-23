dc8e120b645088acc1d33d626dd55c3e6c7dd387
vkmx <vkmx@users.noreply.github.com>
2025-04-23 12:05:04 +0530

    Complete '02_repeatString'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function( string, repeat ) {
+
+    if( repeat < 0 ){
+        return 'ERROR';
+    }
+
+    let result = '';
+
+    for( let i = 0; i < repeat; i++ ){
+        result = result.concat(string);
+    }
+
+    return result;
 
 };
 
