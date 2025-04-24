65524b5f0b73631c201b93bb8bd2c7e8d954f99a
vkmx <vkmx@users.noreply.github.com>
2025-04-24 14:53:06 +0530

    Finish '06_leapYears'

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function( year ) {
 
+    let isLeapYear = false;
+
+    if( year % 4 === 0 ){
+        isLeapYear = true;
+    }
+
+    if( year % 100 === 0 ){
+        isLeapYear = false;
+    }
+
+    if( year % 400 === 0 ){
+        isLeapYear = true;
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
