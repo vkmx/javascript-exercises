d8ab2a157635be068822669352c1c7c29f0583c1
vkmx <vkmx@users.noreply.github.com>
2025-04-24 14:33:20 +0530

    Finish '05_sumAll'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function( num1, num2 ) {
+
+    let result;
+
+    if( Number.isInteger(num1) && Number.isInteger(num2) ) {
+
+        let n = 0;
+
+        if( num2 > num1 ){
+            n = (num2 - num1) + 1;
+        }
+
+        if( num1 > num2 ){
+            n = (num1 - num2) + 1;
+        }
+
+        if( n < 0 || num1 < 0 || num2 < 0 ){
+            result = 'ERROR'
+        } else {
+            result = n / 2 * ( num1 + num2 );
+        }
+
+    } else {
+        result = 'ERROR';
+    }
+
+    return result;
 
 };
 
