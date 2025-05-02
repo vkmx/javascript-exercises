e0d0ff4ef4949bc624e50873e792f08838a4194b
vkmx <vkmx@users.noreply.github.com>
2025-05-02 11:15:49 +0530

    Finish '08_calculator'

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function( num1, num2 ) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function( num1, num2 ) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function( nums ) {
+
+  return nums.reduce( ( accumulator, currentValue ) => accumulator + currentValue, 0 );
 };
 
-const multiply = function() {
-
+const multiply = function( nums ) {
+  return nums.reduce( (acc, curItem ) => acc * curItem, 1 );
 };
 
-const power = function() {
-	
+const power = function( num, power ) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function( num ) {
+  let fact = 1;
+
+  for( let i = 1; i <= num; i++ ){
+    fact = fact * i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
