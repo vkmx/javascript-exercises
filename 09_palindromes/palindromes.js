f8a0eb80094371f510cfb4bf35f6694450d96f07
vkmx <vkmx@users.noreply.github.com>
2025-05-02 11:33:51 +0530

    Finish '09_palindromes'

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function ( string ) {
+    let reversed = string.toLowerCase().split('').filter( ( character ) => /[a-zA-Z0-9]/.test( character ) ).reverse().join('');
 
+    return reversed === string.toLowerCase().replace( /[^a-zA-Z0-9]/g, '' ) ;
 };
 
 // Do not edit below this line
