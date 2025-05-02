ac36f0376dcb3d9b164fc5b834f55ac0815f4a00
vkmx <vkmx@users.noreply.github.com>
2025-05-02 21:51:51 +0530

    Finish '10_fibonacci'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function( positionInSequence ) {
 
+    positionInSequence = Number( positionInSequence );
+
+    if( positionInSequence < 0 ) return 'OOPS';
+
+    let sequence = [0];
+    let counter = 0;
+
+    while ( counter < positionInSequence ){
+        let currentItem = sequence[counter];
+        let penultimateItem = sequence[counter-1];
+
+        if( penultimateItem === undefined ){
+            penultimateItem = 1;
+        }
+
+        let val = currentItem + penultimateItem;
+        sequence.push( val );
+        counter++;
+    }
+
+    return sequence[positionInSequence];
 };
 
 // Do not edit below this line
