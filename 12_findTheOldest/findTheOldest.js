f1041b4bda9b533be48ec3e6e07722ad43526bcf
vkmx <vkmx@users.noreply.github.com>
2025-05-03 11:36:55 +0530

    Finish '12_findTheOldest'

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function( people ) {
+    peopleWithAges = people.map( ( person ) => {
 
+        if( ! person.hasOwnProperty( 'yearOfDeath' ) ) person.yearOfDeath = new Date().getFullYear(); //yes, I just deleted the person
+
+        person.age = new Date( person.yearOfDeath ) - new Date( person.yearOfBirth );
+
+        return person;
+    });
+
+    peopleWithAges.sort( ( a, b ) => {
+        if( a.age < b.age ) return 1;
+        if( a.age > b.age ) return -1;
+        if( a.age === b.age ) return 0;
+
+    } );
+
+    return peopleWithAges[0];
 };
 
 // Do not edit below this line
